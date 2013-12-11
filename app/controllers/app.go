c7cf27f983f6d827bee923fd9fe5eb9f6004ff2d
Connor Tumbleson <connor.tumbleson@gmail.com>
2013-12-11 09:01:02 -0600

    add license, add lang files, tweak display.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,7 @@
 package controllers
 
 import "github.com/robfig/revel"
+import "strings"
 
 type App struct {
 	*revel.Controller
@@ -8,4 +9,17 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+func (c App) About() revel.Result {
+	return c.Render()
+}
+
+func init() {
+	revel.TemplateFuncs["active"] = func(s string) string {
+		if strings.Contains(App.Request.URL.String(), "active") {
+			return "active"
+		}
+		return ""
+	}
 }
